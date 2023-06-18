c42e4b35f96df2efc434ab7b48598f865278c5f9
eadabala <eadabala@gmail.com>
2023-06-18 20:51:35 +0530

    Revert "modified EmpController"
    This reverts commit f731cecbafae10a9d29d8a771aeefce7e2942ea4.

--- src/main/java/com/example/demo/EmpController.java
+++ src/main/java/com/example/demo/EmpController.java
@@ -1,8 +1,6 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,10 +10,5 @@ public class EmpController {
 	@GetMapping
 	public String getEmp() {
 		return "Srinivas";
-	}
-	
-	@PostMapping
-	public Emp createEmp(@RequestBody Emp emp) {
-		return emp;
 	}
 }
