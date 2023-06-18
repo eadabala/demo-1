f731cecbafae10a9d29d8a771aeefce7e2942ea4
eadabala <eadabala@gmail.com>
2023-06-18 20:37:05 +0530

    modified EmpController

--- src/main/java/com/example/demo/EmpController.java
+++ src/main/java/com/example/demo/EmpController.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +12,10 @@ public class EmpController {
 	@GetMapping
 	public String getEmp() {
 		return "Srinivas";
+	}
+	
+	@PostMapping
+	public Emp createEmp(@RequestBody Emp emp) {
+		return emp;
 	}
 }
